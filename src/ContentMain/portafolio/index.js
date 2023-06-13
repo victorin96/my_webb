a89e0d533e244ea2712190cf5193bf6ef92dddc0
Victor <victor.bisquertmoscardo@cieep.com>
2023-06-13 16:54:58 +0200

    a

--- src/ContentMain/portafolio/index.js
+++ src/ContentMain/portafolio/index.js
@@ -1,10 +1,8 @@
-import React, { useState } from "react";
+import React from "react";
 import "./styles.css";
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
 import imgProyectTask from "../resources2/Captura de pantalla (559).png";
-import image from "../resources2/Captura de pantalla (559).png";
-import ProyectComponent from "../cardSingleProy";
 
 const PortafolioComponent = () => {
   const responsive = {
@@ -25,14 +23,15 @@ const PortafolioComponent = () => {
       items: 1,
     },
   };
+
   return (
     <section className="content-portafolio" id="portafolio">
       <div className="container-into-portafolio">
         <div className="row">
           <div className="col-12">
             <div className="skill-bx wow zoomIn">
-              <h2>Portafolio</h2>
-              <p>
+              <h2 className="h2-portafolio">Portafolio</h2>
+              <p className="p-portafolio">
                 Lorem Ipsum is simply dummy text of the printing and typesetting
                 industry.<br></br> Lorem Ipsum has been the industry's standard
                 dummy text.
@@ -44,21 +43,33 @@ const PortafolioComponent = () => {
                 slidesToShow={1}
                 swipeable={true}
               >
-                <div className="item">
-                  <img src={imgProyectTask} alt="Image" />
-                  <h5>Web Development</h5>
+                <div className="item link-card">
+                  <a className="astro-DOHJNAO5" href="#">
+                    <img src={imgProyectTask} alt="Image" />
+                    <h5>Gestor de tareas</h5>
+                  </a>
                 </div>
-                <div className="item">
-                  <img src={imgProyectTask} alt="Image" />
-                  <h5>Brand Identity</h5>
+                <div className="item link-card">
+                  <a className="astro-DOHJNAO5" href="#">
+                    <img src={imgProyectTask} alt="Image" />
+                    <h5>Brand Identity</h5>
+                  </a>
                 </div>
-                <div className="item">
-                  <img src={imgProyectTask} alt="Image" />
-                  <h5>Logo Design</h5>
+                <div className="item link-card">
+                  <a className="astro-DOHJNAO5" href="#">
+                    <img src={imgProyectTask} alt="Image" />
+                    <h5>Logo Design</h5>
+                  </a>
                 </div>
-                <div className="item">
-                  <img src={imgProyectTask} alt="Image" />
-                  <h5>Web Development</h5>
+                <div className="item link-card">
+                  <a className="astro-DOHJNAO5" href="#">
+                    <img
+                      className="img-item link-card"
+                      src={imgProyectTask}
+                      alt="Image"
+                    />
+                    <h5>Web Development</h5>
+                  </a>
                 </div>
               </Carousel>
             </div>
